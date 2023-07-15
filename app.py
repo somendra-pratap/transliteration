3cb7573d0d523363b887d090cb5704a4ce7fc74a
Somendra <somendra.pratapsingh.15@gmail.com>
2023-07-16 02:57:39 +0530

    cors added

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -17,10 +19,20 @@ def upload_file():
     with open(file.filename, 'r') as f:
         file_contents = f.read()
     
-    return file_contents
+    # Get the number of characters in the file
+    num_characters = len(file_contents)
+    
+    # Get the number of capital letters in the file
+    num_capital_letters = sum(1 for char in file_contents if char.isupper())
+    
+    # Prepare the response
+    standard_response = {
+        'standard_answer_score': num_characters,
+        'optimized_answer_score': num_capital_letters,
+        'standard_response': file_contents
+    }
+    
+    return jsonify(standard_response)
 
 if __name__ == '__main__':
     app.run()
-
-
-# curl -X POST -F "file=@/path/to/your/file.txt" http://127.0.0.1:5000/upload
